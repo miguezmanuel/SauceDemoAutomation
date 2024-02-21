0e48d2cfa5bb7e3cceea11c3694c926b79b274fc
Manuel Miguez <manuelmiguezlauria@gmail.com>
2024-02-21 15:52:40 -0300

    amazon choose location abstract page and abstract object created, with methods and elements

---
+++ src/main/java/com/solvd/carina/demo/gui/amazon/components/AmazonChoseLocationCard.java
@@ -0,0 +1,49 @@
+package com.solvd.carina.demo.gui.amazon.components;
+
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.gui.AbstractUIObject;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class AmazonChoseLocationCard extends AbstractUIObject {
+
+    @FindBy(xpath = "//*[@aria-label='Sign in to see your addresses']")
+    private ExtendedWebElement signInButton;
+    @FindBy(xpath = "//*[@aria-label='or enter a US zip code']")
+    private ExtendedWebElement zipCodeInputField;
+    @FindBy(xpath = "//*[@aria-labelledby='GLUXZipUpdate-announce']")
+    private ExtendedWebElement applyButton;
+    @FindBy(css = "select#GLUXCountryList")
+    private ExtendedWebElement selectCountryButton;
+    @FindBy(css = "a#GLUXCountryList_16")
+    private ExtendedWebElement argentinaButton;
+
+    public AmazonChoseLocationCard(WebDriver driver, SearchContext searchContext) {
+        super(driver, searchContext);
+    }
+
+    public boolean isSignInButtonPresent () {
+        return signInButton.isPresent();
+    }
+
+    public void clickSignInButton () {
+        signInButton.click();
+    }
+
+    public void typeZipCodeInputField (String zipCodeInput) {
+        zipCodeInputField.type(zipCodeInput);
+    }
+
+    public void clickApplyButton () {
+        applyButton.click();
+    }
+
+    public void clickSelectCountryButton () {
+        selectCountryButton.click();
+    }
+
+    public void clickArgentinaButton () {
+        argentinaButton.click();
+    }
+}
---
+++ src/main/java/com/solvd/carina/demo/gui/amazon/pages/AmazonChooseLocationPage.java
@@ -0,0 +1,11 @@
+package com.solvd.carina.demo.gui.amazon.pages;
+
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public class AmazonChooseLocationPage extends AbstractPage {
+
+    public AmazonChooseLocationPage(WebDriver driver) {
+        super(driver);
+    }
+}
