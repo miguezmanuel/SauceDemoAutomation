220a250f68062faa8fe906bdd7c4f38586587701
Manuel Miguez <manuelmiguezlauria@gmail.com>
2024-02-23 08:51:51 -0300

    elements from shop navigation bat file updated, xpath created.

--- src/main/java/com/solvd/carina/demo/gui/amazon/components/AmazonShopNavigationBar.java
+++ src/main/java/com/solvd/carina/demo/gui/amazon/components/AmazonShopNavigationBar.java
@@ -4,15 +4,23 @@ import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractUIObject;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
 
 public class AmazonShopNavigationBar extends AbstractUIObject {
 
+    @FindBy(css = "a#nav-hamburger-menu")
     private ExtendedWebElement sideMenuButton;
+    @FindBy(xpath = "//a[@class='nav-a  ' and text()='Today's Deals']")
     private ExtendedWebElement todayDealsButton;
+    @FindBy(xpath = "//a[@class='nav-a  ' and text()='Customer Service']")
     private ExtendedWebElement customerServiceButton;
+    @FindBy(xpath = "//a[@class='nav-a  ' and text()='Registry']")
     private ExtendedWebElement registryButton;
+    @FindBy(xpath = "//a[@class='nav-a  ' and text()='Gift Cards']")
     private ExtendedWebElement giftCardsButton;
+    @FindBy(xpath = "//a[@class='nav-a  ' and text()='Sell']")
     private ExtendedWebElement sellButton;
+
     public AmazonShopNavigationBar(WebDriver driver, SearchContext searchContext) {
         super(driver, searchContext);
     }
