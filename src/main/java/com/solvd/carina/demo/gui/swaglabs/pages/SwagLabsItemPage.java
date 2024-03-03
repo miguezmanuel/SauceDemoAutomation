04a599273d78b92f81cdcef049ffe8f99ef5d0a4
Manuel Miguez <manuelmiguezlauria@gmail.com>
2024-03-02 23:10:00 -0300

    item page class updated with elements and overriden method

--- src/main/java/com/solvd/carina/demo/gui/swaglabs/pages/SwagLabsItemPage.java
+++ src/main/java/com/solvd/carina/demo/gui/swaglabs/pages/SwagLabsItemPage.java
@@ -1,10 +1,24 @@
 package com.solvd.carina.demo.gui.swaglabs.pages;
 
 import com.solvd.carina.demo.gui.swaglabs.common.SwagLabsBasePage;
+import com.solvd.carina.demo.gui.swaglabs.components.SwagLabsHeader;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import org.openqa.selenium.WebDriver;
 
 public class SwagLabsItemPage extends SwagLabsBasePage {
+
+    private ExtendedWebElement backToProductsButton;
+    private ExtendedWebElement addToCartButton;
+    private ExtendedWebElement removeButton;
+    private ExtendedWebElement itemTitle;
+    private ExtendedWebElement itemDescription;
+    private ExtendedWebElement itemPrice;
     public SwagLabsItemPage(WebDriver driver) {
         super(driver);
+    }
+
+    @Override
+    public SwagLabsHeader getHeader() {
+        return super.getHeader();
     }
 }
