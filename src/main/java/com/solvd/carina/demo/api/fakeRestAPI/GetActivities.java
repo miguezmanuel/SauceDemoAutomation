7d4ffdf858469fe8725b4ac73b2e03be0e9bf1eb
Manuel Miguez <manuelmiguezlauria@gmail.com>
2024-03-04 21:48:18 -0300

    created test class, method class updated for getting activities

--- src/main/java/com/solvd/carina/demo/api/fakeRestAPI/GetActivities.java
+++ src/main/java/com/solvd/carina/demo/api/fakeRestAPI/GetActivities.java
@@ -1,12 +1,21 @@
 package com.solvd.carina.demo.api.fakeRestAPI;
 
 import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+import com.zebrunner.carina.utils.config.Configuration;
 
 import java.util.Properties;
 
+@Endpoint(url = "${api_url}/api/v1/Activities", methodType = HttpMethodType.GET)
+@ResponseTemplatePath(path = "test/resources/api/fakeRestAPI/activities/get_activities_rs.json")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class GetActivities extends AbstractApiMethodV2 {
-
-    public GetActivities(String rqPath, String rsPath, Properties properties) {
-        super(rqPath, rsPath, properties);
+    public GetActivities() {
+        replaceUrlPlaceholder("api_url", Configuration.get("api_url").toString());
     }
+
 }
---
+++ src/test/java/com/solvd/carina/demo/api/ApiFakeRestTest.java
@@ -0,0 +1,15 @@
+package com.solvd.carina.demo.api;
+
+import com.solvd.carina.demo.api.fakeRestAPI.GetActivities;
+import com.zebrunner.carina.core.IAbstractTest;
+import org.testng.annotations.Test;
+
+public class ApiFakeRestTest implements IAbstractTest {
+
+    @Test
+    public void getActivitiestTest () {
+        GetActivities getActivities = new GetActivities();
+        getActivities.callAPIExpectSuccess();
+        getActivities.validateResponse();
+    }
+}
